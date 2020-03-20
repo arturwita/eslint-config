85fb994561fdb189f90e88af610e6184c5116602
arturwita <32541872+arturwita@users.noreply.github.com>
2020-03-20 16:48:50 +0100

    airbnb config extended and overrode (#1)
    * airbnb config extended and overrode
    
    * package.json update
    
    * version update
    
    * eslint moved from dev-deps to dependencies
    
    * version -rc2
    
    * strict rule fix
    
    Co-Authored-By: Bartosz Studnik <wellski15@gmail.com>
    
    * version 1.0.0 release
    
    Co-authored-by: Bartosz Studnik <wellski15@gmail.com>

---
+++ index.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const { RULE_OFF, RULE_ON, RULE_WARN_ONLY } = require('./lib/consts');
+
+module.exports = {
+  extends: [
+    'eslint-config-airbnb-base',
+    './lib/overrides',
+  ].map(require.resolve),
+  parserOptions: {
+    sourceType: 'script',
+    ecmaVersion: 2018
+  },
+  plugins: [
+    'node'
+  ],
+  rules: {
+    'no-await-in-loop': RULE_WARN_ONLY,
+    'no-plusplus': RULE_OFF,
+    'arrow-parens': [RULE_ON, 'as-needed'],
+    'no-use-before-define': RULE_OFF,
+    'max-depth': [RULE_ON, 4],
+    'class-methods-use-this': RULE_OFF,
+    'no-underscore-dangle': RULE_OFF,
+    'comma-dangle': [RULE_ON, 'only-multiline'],
+    'arrow-body-style': [RULE_ON, 'as-needed'],
+    'max-len': [RULE_ON,
+      {
+        code: 120,
+        comments: 160,
+        ignoreUrls: true,
+        ignoreTemplateLiterals: true,
+        ignoreStrings: true,
+      }
+    ],
+    'indent': [
+      RULE_ON,
+      4,
+      {
+       'SwitchCase': RULE_WARN_ONLY
+      }
+    ],
+    'max-classes-per-file': RULE_OFF,
+    'no-restricted-syntax': RULE_OFF,
+    'prefer-object-spread': RULE_ON,
+    'strict': [RULE_ON, 'global'],
+    'node/exports-style': [RULE_ON,
+      'module.exports'
+    ],
+    'import/no-extraneous-dependencies': [RULE_ON,
+      {
+        devDependencies: [
+          'test/**',
+          '**/*{test,spec}.js',
+        ]
+      }
+    ],
+    'func-names': RULE_OFF,
+    'no-unused-expressions': RULE_OFF
+  }
+};
